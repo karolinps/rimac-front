e1e1d5300d9a8ada3519a7bf53519a8fee4e2a5e
Karolin APS <porrask92@gmail.com>
2021-10-27 13:02:42 -0500

    create component " "

---
+++ src/components/modules/buttonCounter/ButtonCounterStyles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const WrapperStyled = styled.div`
+  border: 1px solid #c5cbe0;
+  box-sizing: border-box;
+  border-radius: 8px;
+  display: flex;
+  width: 160px;
+  height: 56px;
+  justify-content: space-evenly;
+  align-items: center;
+  color: #6f7dff;
+`;
+
+export const LeftStyled = styled.div``;
+export const CenterStyled = styled.div`
+  font-family: Lato;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: center;
+  letter-spacing: 0.2px;
+  color: #494F66;
+`;
+
+export const RightStyled = styled.div``;
