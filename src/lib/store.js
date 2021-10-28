d7e65c6254483aba9953ba2b95336214aa384d78
Karolin APS <porrask92@gmail.com>
2021-10-28 17:20:35 -0500

    import counter slice

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "@lib/slices/authSlice";
+import counterReducer from "@lib/slices/counterSlice";
 
 export const store = configureStore({
-  reducer: { auth: authReducer },
+  reducer: { auth: authReducer, counter: counterReducer },
 });
