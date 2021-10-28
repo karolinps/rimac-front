9d8725c91512a9da06ea751140857564d672b0c5
Karolin APS <porrask92@gmail.com>
2021-10-28 16:57:43 -0500

    create thunks for user id

--- src/lib/slices/authSlice.js
+++ src/lib/slices/authSlice.js
@@ -1,11 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getByUser } from "@api/user";
 
+export const fetchByUserThunk = createAsyncThunk(
+  "data/fetchByUserThunk",
+  async (id) => {
+    const response = await getByUser(id);
+    const { data } = response;
+    return data;
+  }
+);
 const initialState = {
   user: {
     nro_doc: "",
     phone: "",
     plate: "",
   },
+  status: "",
+  currentUser: {},
 };
 
 export const authSlice = createSlice({
@@ -15,6 +26,18 @@ export const authSlice = createSlice({
     setAuth: (state, { payload }) => {
       state.user = payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchByUserThunk.pending, (state, action) => {
+      state.status = "loading";
+    });
+    builder.addCase(fetchByUserThunk.fulfilled, (state, { payload }) => {
+      state.currentUser = payload;
+      state.status = "succeeded";
+    });
+    builder.addCase(fetchByUserThunk.rejected, (state, action) => {
+      state.status = "failed";
+    });
   },
 });
 
