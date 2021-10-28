f3df188abda8dfc8897d14880bc0cc5dcc2bc7bd
Karolin APS <porrask92@gmail.com>
2021-10-28 16:32:55 -0500

    show placa ingresada from login

--- src/components/modules/card/Card.jsx
+++ src/components/modules/card/Card.jsx
@@ -9,12 +9,16 @@ import {
 } from "./CardStyles";
 
 import menSvg from "@assets/images/men.svg";
+import { useSelector } from "react-redux";
 
 const Card = () => {
+  const { plate } = useSelector((state) => state.auth.user);
+
+  React.useEffect(() => {}, [plate]);
   return (
     <WrapperCardStyled>
       <LeftStyled>
-        <TitleSmall> Placa: C2U-114</TitleSmall>
+        <TitleSmall> Placa: {plate}</TitleSmall>
         <TitleBig>Wolkswagen 2019 Golf</TitleBig>
       </LeftStyled>
       <RightStyled>
